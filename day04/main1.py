c8856c7ae6a73eeb1f3e975de33e27d86163c3d2
stephi <step over>
2024-12-17 03:33:09 -0300

    Day04-P1 completed :) TODO: P2

--- day04/main1.py
+++ day04/main1.py
@@ -13,41 +13,20 @@ def find_xmas():
     sum = 0
 
     n = len(data)
-    m = len(data[0])
+    m = len(data[0])-1
+    directions = [(0,1), (0,-1), (1,0), (-1,0), (-1,1), (-1,-1), (1,-1), (1,1)]
 
     for i in range(n):
         for j in range(m):
             if data[i][j] == 'X':
-                sum += is_horizontal_xmas(i,j,m)
-                sum += is_vertical_xmas(i,j,n)
-                sum += is_diagonal_xmas(i,j,n,m)
+                sum += [next_four(i,di,j,dj,n,m) == "XMAS" for (di,dj) in directions].count(True)
     
     return sum
-                    
-def is_horizontal_xmas(i, j, m):
-    res = (j + 3 < m and data[i][j:j+3] == "XMAS")
-    res += (j > 3 and data[i][j-3:j] == "XMAS")
 
-    return res
-
-def is_vertical_xmas(i, j, n):
-    print(i)
-    print(j)
-    print(n)
-    print(data)
-
-    res = (i + 3 < n and data[i:i+1][j] == "XMAS")
-    res += (i > 3 and data[i-3:i][j] == "XMAS")
-
-    return res
-
-def is_diagonal_xmas(i, j, n, m):
-    res = (i > 3 and j + 3 < m) and [data[i-d][j+d] for d in range(3)] == "XMAS"
-    res += (i > 3 and j > 3) and [data[i-d][j-d] for d in range(3)] == "XMAS"
-    res += (i + 3 < n and j > 3) and [data[i+d][j-d] for d in range(3)] == "XMAS"
-    res += (i + 3 < n and j + 3 < m) and [data[i+d][j+d] for d in range(3)] == "XMAS"
-
-    return res
+def next_four(i, si, j, sj, n, m):
+    letters = [data[i + si*d][j + sj*d] for d in range(4) 
+               if i + si*d in range(n) and j + sj*d in range(m)]
+    return ''.join(letters)
 
 def main():
     save_input()
